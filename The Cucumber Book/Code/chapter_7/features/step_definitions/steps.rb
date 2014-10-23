9cf62a03d3ec46523c5b40f3e2b40e5d0d010762
Sergey Hanchar <hanchar.sergey@gmail.com>
2014-10-23 16:37:10 +0400

    Cucumber - Chapter 8.2

--- The Cucumber Book/Code/chapter_7/features/step_definitions/steps.rb
+++ The Cucumber Book/Code/chapter_7/features/step_definitions/steps.rb
@@ -1,11 +1,5 @@
 World(KnowsMyAccount)
 
-# Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
-#   my_account.deposit(amount)
-#
-#   expect(my_account.balance).to eq(amount), "Expected the balance to be #{amount}"
-# end
-
 When /^I withdraw (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
   teller.withdraw_from(my_account, amount)
 end
