416b137377c8a9021e84c28c1e9bd0f724182db6
Sergey Hanchar <hanchar.sergey@gmail.com>
2014-10-23 15:25:29 +0400

    Cucumber - Chanter 7 - ATM

--- The Cucumber Book/Code/chapter_7/features/step_definitions/steps.rb
+++ The Cucumber Book/Code/chapter_7/features/step_definitions/steps.rb
@@ -1,58 +1,9 @@
-module KnowsMyAccount
-  def my_account
-    @my_account ||= Account.new
-  end
-  
-  def cash_slot
-    @cash_slot ||= CashSlot.new
-  end
-  
-  def teller
-    @teller ||= Teller.new(cash_slot)
-  end
-end
-
 World(KnowsMyAccount)
-
-class Account
-  def deposit(amount)
-    @balance = amount
-  end
-  
-  def balance
-    @balance
-  end
-end
-
-class Teller
-  def initialize(cash_slot)
-    @cash_slot = cash_slot
-  end
-  
-  def withdraw_from(account, amount)
-    @cash_slot.dispense(amount)
-  end
-end
-
-class CashSlot
-  def contents
-    @contents or raise('I am empty!')
-  end
-  
-  def dispense(amount)
-    @contents = amount
-  end
-end
-
-CAPTURE_CASH_AMOUNT = Transform /^\$(\d+)$/ do |digits|
-  digits.to_i
-end
 
 Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
   my_account.deposit(amount)
 
-  expect(my_account.balance).to \
-    eq(amount), "Expected the balance to be #{amount}"
+  expect(my_account.balance).to eq(amount), "Expected the balance to be #{amount}"
 end
 
 When /^I withdraw (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
