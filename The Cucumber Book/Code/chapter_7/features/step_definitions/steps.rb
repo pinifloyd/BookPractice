4dee222340308ddc083a73a6a4abbeec2301ebd6
Sergey Hanchar <hanchar.sergey@gmail.com>
2014-10-23 17:19:23 +0400

    Cucumber - Chapter 8

--- The Cucumber Book/Code/chapter_7/features/step_definitions/steps.rb
+++ The Cucumber Book/Code/chapter_7/features/step_definitions/steps.rb
@@ -1,9 +1,3 @@
-World(KnowsMyAccount)
-
-When /^I withdraw (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
-  teller.withdraw_from(my_account, amount)
-end
-
 Then /^(#{CAPTURE_CASH_AMOUNT}) should be dispensed$/ do |amount|
   expect(cash_slot.contents).to eq(amount)
 end
