a3b1122af635615ea5b57c4bc63729943a398f85
Sergey Hanchar <hanchar.sergey@gmail.com>
2014-10-23 10:22:34 +0400

    Use expect instead should

--- The Cucumber Book/Code/calculator/features/step_definitions/calculator_steps.rb
+++ The Cucumber Book/Code/calculator/features/step_definitions/calculator_steps.rb
@@ -8,6 +8,5 @@ When /^the calculator is run$/ do
 end
 
 Then /^the output should be "([^"]*)"$/ do |expected_output|
-  # @output.should == expected_output
   expect(@output).to eq(expected_output)
 end
