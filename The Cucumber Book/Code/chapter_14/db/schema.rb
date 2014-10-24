d64f0b550633acd7e3784ba3f466ec484dd3d109
Sergey Hanchar <hanchar.sergey@gmail.com>
2014-10-24 14:43:58 +0400

    Cucumber - Chapter 14.5

--- The Cucumber Book/Code/chapter_14/db/schema.rb
+++ The Cucumber Book/Code/chapter_14/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024101547) do
+ActiveRecord::Schema.define(version: 20141024103304) do
+
+  create_table "messages", force: true do |t|
+    t.integer  "user_id"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
--- The Cucumber Book/Code/chapter_14/features/step_definitions/user_steps.rb
+++ The Cucumber Book/Code/chapter_14/features/step_definitions/user_steps.rb
@@ -1,3 +1,13 @@
 Given /^there is a User$/ do
   FactoryGirl.create(:user)
 end
+
+Given /^the User has posted the message "([^"]*)"$/ do |message_text|
+  expect(User.count).to eq(1)
+  FactoryGirl.create(:message, content: message_text, user: User.first)
+end
+
+When /^I visit the page for the User$/ do
+  expect(User.count).to eq(1)
+  visit(user_path(User.first))
+end
