a7b3924be1feb8c177784a852dfab49847a43cee
Sergey Hanchar <hanchar.sergey@gmail.com>
2014-10-24 18:25:29 +0400

    Cucumber - Chapter 15

--- The Cucumber Book/Code/chapter_14/app/models/message.rb
+++ The Cucumber Book/Code/chapter_14/app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ActiveRecord::Base
   belongs_to :user
 
   def self.like(content)
-    content.nil? ? [] : where(['content LIKE ?', "%#{content}%"])
+    content.nil? ? [] : where(['content LIKE ?', "%#{content}%"]) #.reverse #-> for broke test and see reports
   end
 end
