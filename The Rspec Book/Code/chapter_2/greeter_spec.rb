4f25f77e3ae0379b8c4033c80a96b1f90b5bb7bd
Sergey Hanchar <hanchar.sergey@gmail.com>
2014-11-08 12:57:46 +0300

    RSpec - Chapter 2.2

---
+++ The Rspec Book/Code/chapter_2/greeter_spec.rb
@@ -0,0 +1,12 @@
+class RSpecGreeter
+  def greet
+    "Hello RSpec!"
+  end
+end
+
+describe "RSpec Greeter" do
+  it "should say 'Hello RSpec!' when it receives the greet() message" do
+    greeter = RSpecGreeter.new
+    expect(greeter.greet).to eq("Hello RSpec!")
+  end
+end
