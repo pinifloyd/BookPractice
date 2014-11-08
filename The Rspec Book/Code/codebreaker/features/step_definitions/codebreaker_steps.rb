398a3b7556ed000f9e5bc434fe935f9ac7f9e68c
Sergey Hanchar <hanchar.sergey@gmail.com>
2014-11-08 22:59:37 +0300

    RSpec - Chapter 6

--- The Rspec Book/Code/codebreaker/features/step_definitions/codebreaker_steps.rb
+++ The Rspec Book/Code/codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -12,14 +12,33 @@ def terminal_output
   @terminal_output ||= TerminalOutput.new
 end
 
+#
+#  From Starts Game Feature
+#
 Given /^I am not yet playing$/ do
 end
 
 When /^I start a new game$/ do
   game = Codebreaker::Game.new(terminal_output)
-  game.start
+  game.start('1234')
 end
 
 Then /^I should see "([^"]*)"$/ do |message|
   expect(terminal_output.messages).to include(message)
+end
+
+#
+#  From Submits Guess Feature
+#
+Given /^the secret code is "([^"]*)"$/ do |secret|
+  @game = Codebreaker::Game.new(terminal_output)
+  @game.start(secret)
+end
+
+When /^I guess "([^"]*)"$/ do |guess|
+  @game.guess(guess)
+end
+
+Then /^the mark should be "([^"]*)"$/ do |mark|
+  expect(terminal_output.messages).to include(mark)
 end
