5bc70851564b8ac76ebe784386be2d0530f4debd
Sergey Hanchar <hanchar.sergey@gmail.com>
2014-11-08 19:25:42 +0300

    RSpec - Chapter 4

--- The Rspec Book/Code/codebreaker/features/step_definitions/codebreaker_steps.rb
+++ The Rspec Book/Code/codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -1,6 +1,25 @@
+class TerminalOutput
+  def messages
+    @messages ||= []
+  end
+
+  def puts(message)
+    messages << message
+  end
+end
+
+def terminal_output
+  @terminal_output ||= TerminalOutput.new
+end
+
 Given /^I am not yet playing$/ do
 end
 
 When /^I start a new game$/ do
-  Codebreaker::Game.new.start
+  game = Codebreaker::Game.new(terminal_output)
+  game.start
+end
+
+Then /^I should see "([^"]*)"$/ do |message|
+  expect(terminal_output.messages).to include(message)
 end
