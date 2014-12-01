6f86ff93985254e068b8919e2050041b59503613
Sergey Hanchar <hanchar.sergey@gmail.com>
2014-12-01 17:54:10 +0300

    Rename all folders

--- Agile Web Development with Rails 4th Ed/app/helpers/application_helper.rb
+++
@@ -1,8 +0,0 @@
-module ApplicationHelper
-
-  def hidden_div_if(condition, attributes = {}, &block)
-    attributes[:style] = 'display: none' if condition
-    content_tag(:div, attributes, &block)
-  end
-
-end
--- Agile Web Development with Rails 4th Ed/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RailsAgile::Application.config.secret_token = '7564f6e549974ec3c1f4f499c965716fc28748031343693c7a94800d5b00a40a31a8fd000b9705c0f131b7668477ed47407d987030b2b84d67c4ec63d3c5a108'
